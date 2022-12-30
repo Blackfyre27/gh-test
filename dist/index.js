7606d15a0f17f8bbb9e1a8311de63f44e5037ae1
judecabodil22 <31956023+judecabodil22@users.noreply.github.com>
2022-12-30 15:37:33 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,14 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-async function run() {
-  console.log('Hello, world!');
-}
+let query = "mutation { create_item (board_id: 3688894632, group_id: \"topics\", item_name: \"new item\") { id }}";
 
-run();
-
-module.exports = __webpack_exports__;
-/******/ })()
-;
+fetch ("https://api.monday.com/v2", {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIxNDU1MzE2NywidWlkIjozNzQ3NzQyNSwiaWFkIjoiMjAyMi0xMi0yOVQwNjoyOTozMy4wNjJaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQ1MTg5MDEsInJnbiI6InVzZTEifQ.4YqSJlQLAFyBbd4nfU2mbInZ38ntoS_AU8zlJQAGVBs'
+   },
+   body: JSON.stringify({
+     query : query
+   })
+  })
+   .then(res => res.json())
+   .then(res => console.log(JSON.stringify(res, null, 2)));
