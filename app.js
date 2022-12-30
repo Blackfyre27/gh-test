ad8f21821a9c852cc04ad7143daa1ae29adc492c
judecabodil22 <31956023+judecabodil22@users.noreply.github.com>
2022-12-30 20:41:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var test = "'" +  "text\" + "'";
+//var test = "'" +  "text\" + "'";
 
 let query = `mutation {create_item (item_name:jec, board_id:3688894632,value: \"{\\\"text\\\":\\\"google.com\\\"}\") {id}}`;
 //let query = "mutation {create_item (item_name:"+"jec"+", board_id:3688894632,column_values: "+"{\"+" test :\"google.com\"}") {id}}";
