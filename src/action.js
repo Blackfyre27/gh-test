d5199a8e187a3e054d87449386dfa49e6a036fc2
judecabodil22 <31956023+judecabodil22@users.noreply.github.com>
2022-12-30 15:33:33 +0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,5 +1,14 @@
-async function run() {
-  console.log('Hello, world!');
-}
+let query = "mutation { create_item (board_id: 3688894632, group_id: \"topics\", item_name: \"new item\") { id }}";
 
-run();
+fetch ("https://api.monday.com/v2", {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIxNDU1MzE2NywidWlkIjozNzQ3NzQyNSwiaWFkIjoiMjAyMi0xMi0yOVQwNjoyOTozMy4wNjJaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQ1MTg5MDEsInJnbiI6InVzZTEifQ.4YqSJlQLAFyBbd4nfU2mbInZ38ntoS_AU8zlJQAGVBs'
+   },
+   body: JSON.stringify({
+     query : query
+   })
+  })
+   .then(res => res.json())
+   .then(res => console.log(JSON.stringify(res, null, 2)));
